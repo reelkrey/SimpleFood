3f8c9d8f1eb2792fa5c5917d1e0d8e94db578ee6
reelkrey <reelkrey@gmail.com>
2022-12-03 11:44:32 +0000

    added catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,26 @@
 $(function(){
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  $('.select-style').styler();
+
   $('.reviews__slider').slick({
       dots: true,
       arrows: true,
       infinite: false,
-      prevArrow: '<button type="button" class="reviews-arrow reviews-arrow--prev"><svg><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
-      nextArrow: '<button type="button" class="reviews-arrow reviews-arrow--next"><svg><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
+      prevArrow: '<button type="button" class="reviews__arrow reviews__arrow--prev"><svg><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
+      nextArrow: '<button type="button" class="reviews__arrow reviews__arrow--next"><svg><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
       autoplay: true,
       autoplaySpeed: 10000
   });
@@ -22,68 +37,42 @@ $(function(){
         slidesToShow: 1
       });
     } else {
-      $('.restaurants__list').slick('unslick');
+      $('.restaurants__list.slick-initialized').slick('unslick');
     }
   });
 });
   
 var mixer = mixitup('.pop-categories__content');
-  
-  
-document.addEventListener('DOMContentLoaded', () => {	
-  
-  const burger = document.querySelector('.burger'); 
-  
-  burger.addEventListener('click', () => {
-  
-  burger.classList.toggle('burger--active'); 
-  });
-});
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const burger = document.querySelector('.burger');
+const bodyLock = document.querySelector('body');
+const burgerClose = document.querySelector('.burger-mobile');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  const bodyLock = document.querySelector('body');
 
-  burger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('mobile-menu--active'); 
-    if (mobileMenu.classList.contains('mobile-menu--active')) {
-      burger.classList.add('burger--active');
-      bodyLock.classList.add('lock'); 
-    }
-    else {
-      burger.classList.remove('burger--active');
-      bodyLock.classList.remove('lock'); 
-    }
+  burger.addEventListener('click',  () => {
+    mobileMenu.classList.add('mobile-menu--active');
+    bodyLock.classList.add('lock');
+  });
+
+  burgerClose.addEventListener('click', () => {
+    mobileMenu.classList.remove('mobile-menu--active');
+    bodyLock.classList.remove('lock');
   });
 });
 
-document.addEventListener('click', function (e) {
-  if (e.target !== burger && e.target !== mobileMenu) {
-    burger.classList.remove('burger--active');
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_menu = target == mobileMenu || mobileMenu.contains(target);
+  let its_hamburger = target == burger;
+  let menu_is_active = mobileMenu.classList.contains('mobile-menu--active');
+
+  if (!its_menu && !its_hamburger && menu_is_active) {
     mobileMenu.classList.remove('mobile-menu--active');
     bodyLock.classList.remove('lock');
   }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerMobile = document.querySelector('.burger--mobile');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  const bodyLock = document.querySelector('body');
-
-  burgerMobile.addEventListener('click', () => {
-    mobileMenu.classList.toggle('mobile-menu--active');
-    bodyLock.classList.remove('lock');
-  });
-});
-
-document.addEventListener('DOMContentLoaded', () => {	
+})
   
-  const burgerMobile = document.querySelector('.burger--mobile'); 
-  const burgerDesk = document.querySelector('.burger--desktop')
   
-  burgerMobile.addEventListener('click', () => {
-  
-  burgerDesk.classList.remove('burger--active'); 
-  });
-});
+
--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,8 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js', 
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'app/js/main.js'])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
@@ -69,17 +71,17 @@ function cleanDist() {
 }
 
 function svgSprites() {
-    return src('app/images/icons/*.svg')
+    return src('app/images/icons/*.svg') 
     .pipe(
-        svgSprite({
-          mode: {
-            stack: {
-              sprite: '../sprite.svg',
+          svgSprite({
+            mode: {
+              stack: {
+                sprite: '../sprite.svg', 
+              },
             },
-          },
-        })
-      )
-    .pipe(dest('app/images'));
+          })
+        )
+    .pipe(dest('app/images')); 
 }
 
 const htmlInclude = () => {
@@ -110,4 +112,4 @@ exports.svgSprites   = svgSprites;
 exports.htmlInclude  = htmlInclude;
 exports.build        = series(cleanDist, images, build);
 
-exports.default      = parallel(htmlInclude, svgSprites, styles, scripts, browsersync, watching);
+exports.default      = parallel(htmlInclude, svgSprites, styles, scripts, browsersync, watching,);
